3edffa624bdf70ab10175cf201a64dc0f8037c13
Roman <barnaulromantv@gmail.com>
2026-08-14 15:22:41 +0700

    Added server IP to ServerInfo.java

---
+++ ServerInfo.java
@@ -0,0 +1,11 @@
+// Minecraft Server 1.12.2
+// Моды: IndustrialCraft 2, BuildCraft, Forestry
+// Автор: BarnaulRomanTV
+
+public class ServerInfo {
+    public static void main(String[] args) {
+        System.out.println("BarnaulRomanTV Minecraft Server 1.12.2");
+        System.out.println("Mods: IC2 + BuildCraft + Forestry");
+        System.out.println("IP для подключения: katherine-lineage.tun.ply.gg:25565");
+    }
+}
